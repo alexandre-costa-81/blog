71213031b610533abed1de7de17640ac94273888
Alexandre <alexandre.gcosta@gmail.com>
2016-02-28 12:02:00 -0300

    corrige rota

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-}).get('/sobre', function(req, res, next) {
+});
+
+router.get('/sobre', function(req, res, next) {
   res.render('sobre', { title: 'Sobre' });
 });
 
