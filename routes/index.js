7804abc9ca686090cd9f263586cb6c24740fd934
Alexandre <alexandre.gcosta@gmail.com>
2016-02-28 11:44:03 -0300

    adiconado layout do bootstrap

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+}).get('/sobre', function(req, res, next) {
+  res.render('sobre', { title: 'Sobre' });
+});
+
+router.get('/contato', function(req, res, next) {
+  res.render('contato', { title: 'Contato' });
 });
 
 module.exports = router;
